f949a9ffd2d53fcfb0b8bd90357dfee4008ea77d
Isap <isap@outlook.com>
2020-08-14 13:19:58 -0700

    docs agregado

---
+++ docs/precache-manifest.f18b2b33fce73c7ff6be6228f90299e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebebdc9d0619e5fd3e4373dda6c6ceb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5188b4beff41ec131c77",
+    "url": "/static/css/main.3dfa9ba6.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5188b4beff41ec131c77",
+    "url": "/static/js/main.4d723567.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
